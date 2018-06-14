adf4c7e335f85b813ba763bd53bf98e4de81f093
donrestarone <shashikejayatunge@gmail.com>
2018-06-14 15:41:10 -0400

    pass props from app.js to movie.js

--- lord-of-the-rings/src/Movie.js
+++ lord-of-the-rings/src/Movie.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
-
+// props are coming here from app.js (prop names; title, hours, minutes)
 class Movie extends Component {
     render() {
         return (
             <div>
                 <h1>The Lord of the Rings; A Trilogy</h1>
+                <p>{this.props.title}</p>
                 <p>4h 37min</p>
             </div>
         )
