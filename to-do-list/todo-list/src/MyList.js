06fbd81c201f1961c323da5e611eaaedcd253d32
donrestarone <shashikejayatunge@gmail.com>
2018-07-02 16:18:49 -0400

    finish todo

--- to-do-list/todo-list/src/MyList.js
+++ to-do-list/todo-list/src/MyList.js
@@ -7,8 +7,26 @@ class MyList extends Component {
   constructor(props) {
     super()
     this.state = {
-      toDoItemArray: props.theList
+      toDoItemArray: props.theList,
+      newItem: ''
+
     }
+  }
+
+  newItemChange(e) {
+    this.setState({
+      newItem: e.target.value
+    })
+  }
+
+  addItem(e) {
+    e.preventDefault()
+    var list = this.state.toDoItemArray
+    // new item is the variable created by the form submission. it is added to the list array
+    list.push(this.state.newItem)
+    // update the state with the new item
+    // by assigning newitem a blank string you clear out the input field
+    this.setState({toDoItemArray: list, newItem: ''})
   }
   clearList(e) {
     this.setState({
@@ -26,6 +44,14 @@ class MyList extends Component {
           {toDoItems}
         </ul>
         <button onClick={(e) => this.clearList(e)}>Finished the list!</button>
+        <form>
+          <input type="text"
+            placeholder="Type an item here"
+            onChange={(e) => this.newItemChange(e)}
+            value={this.state.newItem}
+          />
+          <button onClick={(e) => this.addItem(e)}>Add it!</button>
+        </form>
       </ div> 
     );
   }
